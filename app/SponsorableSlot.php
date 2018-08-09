9866da2196e916bce6a57a9752abc9a67e2ffe2d
Ryan Ebbers <ebbers.seo@gmail.com>
2018-08-09 17:56:23 -0400

    allowing mass assignment for the SponsorableSlot model

--- app/SponsorableSlot.php
+++ app/SponsorableSlot.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SponsorableSlot extends Model
 {
+    protected $guarded = [];
+
     public function scopeSponsorable($query)
     {
         return $query->whereNull('sponsorship_id')->where('publish_date', '>', now());

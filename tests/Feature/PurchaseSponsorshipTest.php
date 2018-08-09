52bc618cabfb78820625f5b3d87402afe720072f
Ryan Ebbers <ebbers.seo@gmail.com>
2018-08-09 17:37:37 -0400

    setup the initial test assertions for purchasing_available_sponsorship_slots() test

--- tests/Feature/PurchaseSponsorshipTest.php
+++ tests/Feature/PurchaseSponsorshipTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Sponsorable;
+use App\Sponsorship;
 use App\SponsorableSlot;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -16,21 +17,21 @@ class PurchaseSponsorshipTest extends TestCase
     /** @test */
     public function purchasing_available_sponsorship_slots()
     {
-        // $sponsorable = factory(Sponsorable::class)->create(['slug' => 'full-stack-radio']);
+        $sponsorable = factory(Sponsorable::class)->create(['slug' => 'full-stack-radio']);
 
-        $slotA = factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable,]);
-        $slotB = factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable,]);
-        $slotC = factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable,]);
+        $slotA = factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable, 'publish_date' => now()->addMonths(1)]);
+        $slotB = factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable, 'publish_date' => now()->addMonths(2)]);
+        $slotC = factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable, 'publish_date' => now()->addMonths(3)]);
 
-        $response = $this->get('/full-stack-radio/sponsorships/new');
+        $response = $this->postJson('/full-stack-radio/sponsorships');
 
         $response->assertSuccessful();
 
-        $this->assertEquals(1, Purchase::count());
-        $purchase = Purchase::first();
+        $this->assertEquals(1, Sponsorship::count());
+        $sponsorship = Sponsorship::first();
 
-        $this->assertEquals($purchase->getKey(), $slotA->fresh()->purchase_id);
-        $this->assertEquals($purchase->getKey(), $slotC->fresh()->purchase_id);
+        $this->assertEquals($sponsorship->getKey(), $slotA->fresh()->sponsorship_id);
+        $this->assertEquals($sponsorship->getKey(), $slotC->fresh()->sponsorship_id);
 
         $this->assertNull($slotB->fresh()->purchase_id);
     }

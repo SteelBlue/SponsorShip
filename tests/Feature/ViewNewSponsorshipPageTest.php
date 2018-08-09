92a9f3bac4442b07319c13daa89646f8ea196bf2
Ryan Ebbers <ebbers.seo@gmail.com>
2018-08-09 17:13:16 -0400

    added explicit publish_date to all test data

--- tests/Feature/ViewNewSponsorshipPageTest.php
+++ tests/Feature/ViewNewSponsorshipPageTest.php
@@ -6,7 +6,6 @@ use App\Purchase;
 use Tests\TestCase;
 use App\Sponsorable;
 use App\SponsorableSlot;
-use Illuminate\Support\Carbon;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
@@ -21,9 +20,9 @@ class ViewNewSponsorshipPageTest extends TestCase
         $sponsorable = factory(Sponsorable::class)->create(['slug' => 'full-stack-radio']);
 
         $sponsorableSlots = new EloquentCollection([
-            factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable]),
-            factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable]),
-            factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable]),
+            factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable, 'publish_date' => now()->addMonths(1)]),
+            factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable, 'publish_date' => now()->addMonths(2)]),
+            factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable, 'publish_date' => now()->addMonths(3)]),
         ]);
 
         $response = $this->get('/full-stack-radio/sponsorships/new');
@@ -38,9 +37,12 @@ class ViewNewSponsorshipPageTest extends TestCase
     {
         $sponsorable = factory(Sponsorable::class)->create(['slug' => 'full-stack-radio']);
 
-        $slotA = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->addDays(10), 'sponsorable_id' => $sponsorable, ]);
-        $slotB = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->addDays(30), 'sponsorable_id' => $sponsorable, ]);
-        $slotC = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->addDays(3), 'sponsorable_id' => $sponsorable, ]);
+        $slotA = factory(SponsorableSlot::class)
+                    ->create(['publish_date' => now()->addDays(10), 'sponsorable_id' => $sponsorable, ]);
+        $slotB = factory(SponsorableSlot::class)
+                    ->create(['publish_date' => now()->addDays(30), 'sponsorable_id' => $sponsorable, ]);
+        $slotC = factory(SponsorableSlot::class)
+                    ->create(['publish_date' => now()->addDays(3), 'sponsorable_id' => $sponsorable, ]);
 
         $response = $this->get('/full-stack-radio/sponsorships/new');
 
@@ -57,10 +59,14 @@ class ViewNewSponsorshipPageTest extends TestCase
     {
         $sponsorable = factory(Sponsorable::class)->create(['slug' => 'full-stack-radio']);
 
-        $slotA = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->subDays(10), 'sponsorable_id' => $sponsorable, ]);
-        $slotB = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->subDays(1), 'sponsorable_id' => $sponsorable, ]);
-        $slotC = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->addDays(1), 'sponsorable_id' => $sponsorable, ]);
-        $slotD = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->addDays(10), 'sponsorable_id' => $sponsorable, ]);
+        $slotA = factory(SponsorableSlot::class)
+                    ->create(['publish_date' => now()->subDays(10), 'sponsorable_id' => $sponsorable, ]);
+        $slotB = factory(SponsorableSlot::class)
+                    ->create(['publish_date' => now()->subDays(1), 'sponsorable_id' => $sponsorable, ]);
+        $slotC = factory(SponsorableSlot::class)
+                    ->create(['publish_date' => now()->addDays(1), 'sponsorable_id' => $sponsorable, ]);
+        $slotD = factory(SponsorableSlot::class)
+                    ->create(['publish_date' => now()->addDays(10), 'sponsorable_id' => $sponsorable, ]);
 
         $response = $this->get('/full-stack-radio/sponsorships/new');
 
@@ -77,10 +83,14 @@ class ViewNewSponsorshipPageTest extends TestCase
         $sponsorable = factory(Sponsorable::class)->create(['slug' => 'full-stack-radio']);
         $purchase = factory(Purchase::class)->create();
 
-        $slotA = factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable]);
-        $slotB = factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable, 'purchase_id' => $purchase]);
-        $slotC = factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable, 'purchase_id' => $purchase]);
-        $slotD = factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable]);
+        $slotA = factory(SponsorableSlot::class)
+                    ->create(['sponsorable_id' => $sponsorable, 'publish_date' => now()->addMonths(1)]);
+        $slotB = factory(SponsorableSlot::class)
+                    ->create(['sponsorable_id' => $sponsorable, 'publish_date' => now()->addMonths(2), 'purchase_id' => $purchase]);
+        $slotC = factory(SponsorableSlot::class)
+                    ->create(['sponsorable_id' => $sponsorable, 'publish_date' => now()->addMonths(3), 'purchase_id' => $purchase]);
+        $slotD = factory(SponsorableSlot::class)
+                    ->create(['sponsorable_id' => $sponsorable, 'publish_date' => now()->addMonths(4)]);
 
         $response = $this->get('/full-stack-radio/sponsorships/new');
 
